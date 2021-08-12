54297ee1fa28ef40b1fe7b15e3055f115d69572b
ruixuchen <958613616@qq.com>
2021-08-12 20:28:37 +0800

    add some comments

--- src/main/java/com/crx/gitdemo/GitDemo.java
+++ src/main/java/com/crx/gitdemo/GitDemo.java
@@ -17,7 +17,10 @@ package com.crx.gitdemo;
  *
  */
 public class GitDemo {
-
+    /**
+     * add some comments to test git
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello git");
     }
