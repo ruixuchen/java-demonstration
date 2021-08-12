741509e90202c102e6fb040d4f6eb91bc0cc96f9
chenruixu <chenruixu@css.com.cn>
2021-08-12 20:16:43 +0800

    commit a git demo file

---
+++ src/main/java/com/crx/gitdemo/GitDemo.java
@@ -0,0 +1,24 @@
+package com.crx.gitdemo;
+
+/**
+ *
+ * @project 云化电子税务局项目
+ * @package com.crx.gitdemo
+ * @file GitDemo 创建时间:8/12/2021 8:11 PM
+ * @title 标题（要求能简洁地表达出类的功能和职责）
+ * @description 描述（简要描述类的职责、实现方式、使用注意事项等）
+ * @copyright Copyright (c) 2019 中国软件与技术服务股份有限公司
+ * @company 中国软件与技术服务股份有限公司
+ * @module 模块: 模块名称
+ * @author chenruixu
+ * @reviewer 审核人
+ * @version 1.0
+ * @history 修订历史（历次修订内容、修订人、修订时间等）
+ *
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
